6a545cfb53154929046ff47f9b7c046b4eeccda4
comarado <comarado@gmail.com>
2024-11-04 16:31:21 -0300

    done part 2.1

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,16 +1,21 @@
 package hexlet.code;
 import java.util.Scanner;
+import hexlet.code.Cli;
 
 public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the Brain Games!");
-        System.out.println("May I have your name?");
-        String userName = scanner.next();
-        System.out.println("Hello, " + userName + "!");
-
-        scanner.close();
-
+        Cli.greeting();
     }
 
+    static class Cli {
+        public static void greeting() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Welcome to the Brain Games!");
+            System.out.println("May I have your name?");
+            String userName = scanner.next();
+            System.out.println("Hello, " + userName + "!");
+
+            scanner.close();
+        }
+    }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,4 +1,16 @@
 package hexlet.code;
+import java.util.Scanner;
 
 public class Cli {
-}
+        private String name;
+
+        Cli() {
+            this.name = name;
+        }
+
+
+    }
+
+
+
+
